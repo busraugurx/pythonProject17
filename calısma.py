b80fb14b5fb028ce7860ae4b356ed720d41a5eb1
busraugurx <busraugur934@gmail.com>
2021-05-26 16:42:20 +0300

    oyuncu kaydetme

--- calısma.py
+++ calısma.py
@@ -1,2 +1,11 @@
-print(input("bir sayı giriniz:"))
-print(input("lütfen isminizi girin: "))
+print("Hoşgeldiniz\nOyuncu Kaydetme Programı")
+
+oyuncunun_adı=input("Oyuncu Adını girin: ")
+oyuncunun_soyadı=input("Oyuncunun Soyadını Girin:")
+oyuncunun_takımı=input("Oyuncunun Takımını Girin: ")
+
+print("bilgiler kaydediliyor...")
+
+Bilgiler=[oyuncunun_adı,oyuncunun_soyadı,oyuncunun_takımı]
+
+print("OYUNCUNUN ADI: {}\nOYUNCUNUN SOYADI: {}\nOYUNCUNUN TAKIMI: {}\n" .format(Bilgiler[0],Bilgiler[1],Bilgiler[2]))
